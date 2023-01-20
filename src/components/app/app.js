98a45783d2e2434f285f1e088df7d88328ab4ebf
AndrewChumakov <andrew.chumakov12@gmail.com>
2023-01-20 18:04:27 +0300

    переработан компонент App в классовый

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,36 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-const data = [
-  {name: "Alex", salary: 1000, increase: false},
-  {name: "Andrew", salary: 1000, increase: true},
-  {name: "Alex", salary: 1000, increase: false},
-  {name: "Andrew", salary: 1000, increase: false}
-]
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        {name: "Alex", salary: 1000, increase: false},
+        {name: "Andrew", salary: 1000, increase: true},
+        {name: "Alex", salary: 1000, increase: false},
+        {name: "Andrew", salary: 1000, increase: false}
+      ]
+    }
+  }
 
-function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList employees={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+  render() {
+    const {data} = this.state
+    return (
+      <div className="app">
+          <AppInfo />
+  
+          <div className="search-panel">
+              <SearchPanel/>
+              <AppFilter/>
+          </div>
+          
+          <EmployeesList employees={data}/>
+          <EmployeesAddForm/>
+      </div>
+    )
+  }
 }
 
 export default App;
